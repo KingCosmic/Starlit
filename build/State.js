c654ddef1792ea24aac4b67fd4bfde6c8481305a
KingCosmic <KingCosmicDev@gmail.com>
2021-09-20 14:06:48 -0500

    added build to gitignore

--- build/State.js
+++
@@ -1,5 +0,0 @@
-import { Collection } from 'discord.js';
-class BotState {
-}
-BotState.commands = new Collection();
-export default BotState;
--- build/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    token: 'NDM3NjY1MTM0OTYyNTQwNTc0.WtzFaw.5iCXSHV0PWXKL2-a_zsKO-_wiZU',
-    clientID: '437665134962540574',
-    testGuildID: '765500480104300585',
-    isInDevelopment: true
-};
-export default config;
